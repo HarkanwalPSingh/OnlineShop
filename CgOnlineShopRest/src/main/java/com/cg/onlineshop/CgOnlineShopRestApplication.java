156640b1d1f6837d45f817efab149465f7033529
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13072.corp.capgemini.com>
2018-12-03 12:50:58 +0530

    Initial commit of OnlineShop done by 'harkanwal' at 03/12/2018

---
+++ CgOnlineShopRest/src/main/java/com/cg/onlineshop/CgOnlineShopRestApplication.java
@@ -0,0 +1,12 @@
+package com.cg.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgOnlineShopRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgOnlineShopRestApplication.class, args);
+	}
+}
